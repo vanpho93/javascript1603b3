43573bd896da541a4a80b3983c6b5830b89ffce8
Admin <vanpho02@gmail.com>
2017-03-21 20:34:39 +0700

    xong

--- obj.js
+++ obj.js
@@ -2,71 +2,75 @@
 
 const obj = {
     name: 'KhoaPham',
-    age: 30
+    age: 30,
+    show: function(){
+        console.log(this.name + ' :: ' + this.age);
+        return 123;
+    }
 };
-
-console.log(obj);
+console.log(obj.show());
+// console.log(obj);
 
 //Tao qua lop doi tuong
 // function class
 
-function Employee(ten, tuoi) {
-    this.name = ten;
-    this.age = tuoi;
-    this.addAge = function(){
-        this.age++;
-    }
-}
+// function Employee(ten, tuoi) {
+//     this.name = ten;
+//     this.age = tuoi;
+//     this.addAge = function(){
+//         this.age++;
+//     }
+// }
 
-const pho = new Employee('PHO', 18);
-console.log(pho);
-pho.addAge();
-console.log(pho);
-// console.log(pho.name);
+// const pho = new Employee('PHO', 18);
+// console.log(pho);
+// pho.addAge();
+// console.log(pho);
+// // console.log(pho.name);
 
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
+// function Point(x, y) {
+//     this.x = x;
+//     this.y = y;
 
-    this.show = function(){
-        console.log('Chao ' + x + ' ' + y);
-    }
-}  
+//     this.show = function(){
+//         console.log('Chao ' + x + ' ' + y);
+//     }
+// }  
 
-const A = new Point(0, 0);
-A.show();
-console.log(A);
+// const A = new Point(0, 0);
+// A.show();
+// console.log(A);
 
-function Line(a, b) {
-    this.a = a;
-    this.b = b;
-    this.getLength = function() {
-        const dx = this.b.x - this.a.x;
-        const dy = this.b.y - this.a.y;
-        return Math.sqrt(dx*dx + dy*dy);
-    }
-}
+// function Line(a, b) {
+//     this.a = a;
+//     this.b = b;
+//     this.getLength = function() {
+//         const dx = this.b.x - this.a.x;
+//         const dy = this.b.y - this.a.y;
+//         return Math.sqrt(dx*dx + dy*dy);
+//     }
+// }
 
-function Triagle(diemA, diemB, diemC) {
-    this.diemA = diemA;
-    this.diemB = diemB;
-    this.diemC = diemC;
+// function Triagle(diemA, diemB, diemC) {
+//     this.diemA = diemA;
+//     this.diemB = diemB;
+//     this.diemC = diemC;
 
-    this.getChuVi = function() {
-        const AB = new Line(this.diemA, this.diemB);
-        const BC = new Line(this.diemB, this.diemC);
-        const AC = new Line(this.diemA, this.diemC);
-        return AB.getLength() + BC.getLength() + AC.getLength();
-    }
-}
+//     this.getChuVi = function() {
+//         const AB = new Line(this.diemA, this.diemB);
+//         const BC = new Line(this.diemB, this.diemC);
+//         const AC = new Line(this.diemA, this.diemC);
+//         return AB.getLength() + BC.getLength() + AC.getLength();
+//     }
+// }
 
-const C = new Point(0, 0);
-const D = new Point(1, 0);
-const E = new Point(0, 1);
+// const C = new Point(0, 0);
+// const D = new Point(1, 0);
+// const E = new Point(0, 1);
 
-const CDE = new Triagle(C, D, E);
-console.log('CHU VI', CDE.getChuVi());
+// const CDE = new Triagle(C, D, E);
+// console.log('CHU VI', CDE.getChuVi());
 
-const CD = new Line(C, D);
-console.log('KET QUA:', CD.getLength())
-console.log(JSON.stringify(CD));
+// const CD = new Line(C, D);
+// console.log('KET QUA:', CD.getLength())
+// console.log(JSON.stringify(CD));
